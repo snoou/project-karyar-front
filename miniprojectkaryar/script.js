d6a22c74581f0c9238b2f6849d1680bf8aae735b
SAJJAD <122774486+snoou@users.noreply.github.com>
2025-03-29 13:43:41 +0330

    Add files via upload

--- miniprojectkaryar/script.js
+++ miniprojectkaryar/script.js
@@ -1,7 +1,16 @@
 const searchIcon = document.getElementById('search-icon')
 const search = document.getElementById('search')
 const item = document.getElementById('item')
+const close = document.getElementById('close')
 searchIcon.addEventListener('click' , function(){
     item.style.display  = 'none'
     search.style.display = 'block'
+    close.style.display = 'block'
+})
+
+close.addEventListener('click' , function(){
+    search.style.display = 'none'
+    item.style.display  = 'flex'
+    close.style.display = 'none'
+
 })
